a0ddcd5121c83441d9d34cd410b83c6b0c209e8f
Ajay Kumar <visibleajay@gmail.com>
2017-12-21 16:25:57 +0530

    Use message service for updating messages on the frontend.

--- confri-angular/src/app/app.component.ts
+++ confri-angular/src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ElementRef } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material';
 import 'rxjs/add/operator/map';
-
+import {Observable} from 'rxjs';
 import {UserNameDialogComponent} from './user-name-dialog/user-name-dialog.component';
 import {Message} from './model/message.model';
 
 import {ChatService} from './service/chat.service';
+import { MessagesService } from './service/message.service';
 
 @Component({
   selector: 'app-root',
@@ -16,22 +17,37 @@ export class AppComponent implements OnInit, OnDestroy{
 
   private counter:number;
   username: String;
-  messages: Message[];
+  messages: Observable<Message[]>;
   connection;
 
-  constructor(public dialog: MatDialog, private chatservice: ChatService) {}
+  constructor(public dialog: MatDialog, private messageService: MessagesService, private chatservice: ChatService, private el: ElementRef) {}
 
   ngOnInit() {
     this.counter = 0;
-    this.messages = [];
     this.openDialog();
     this.receiveSlackMessage();
+    this.messages = this.messageService.messages;
+
+    this.messages
+    .subscribe(
+      (messages: Array<Message>) => {
+        setTimeout(() => {
+          this.scrollToBottom();
+        });
+      });
+  }
+
+  private scrollToBottom() {
+    const scrollPane: any = this.el
+            .nativeElement.querySelector('.msg-container');
+    scrollPane.scrollTop = scrollPane.scrollHeight;
   }
 
   private receiveSlackMessage() {
     this.connection = this.chatservice.getMessages().subscribe(message => {
       const MESSAGE: Message = new Message(++this.counter, message['username'], message['message'], false);
-      this.messages.push(MESSAGE); 
+      // this.messagesS.push(MESSAGE);
+      this.messageService.addMessage(MESSAGE);
     });
   }
 
@@ -48,7 +64,7 @@ export class AppComponent implements OnInit, OnDestroy{
 
   postMessages(message: String) {
     const MESSAGE: Message = new Message(++this.counter, this.username, message, true);
-    this.messages.push(MESSAGE);
+    this.messageService.addMessage(MESSAGE);
     this.chatservice.sendMessage(message, this.username);
   }
 
@@ -56,9 +72,4 @@ export class AppComponent implements OnInit, OnDestroy{
     this.connection.unsubscribe();
   }
 
-  // makeBackendRequest() {
-    // this.http.get('http://localhost:8080/getData')
-    //         .map( (data) => data)
-    //         .subscribe(data => console.log(data));
-  // }
 }
