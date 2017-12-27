363eb00271adbbe6c706da0d59c67a348aea3605
Ajay Kumar <visibleajay@gmail.com>
2017-12-27 17:45:45 +0530

    improve display of insert message box.
    Improved input message box styling  by taking all available parent height.

--- confri-angular/src/app/input-message/input-message.component.ts
+++ confri-angular/src/app/input-message/input-message.component.ts
@@ -3,7 +3,13 @@ import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'input-message',
-    templateUrl: './input-message.component.html'
+    templateUrl: './input-message.component.html',
+    styles: [`
+                .parentHeightDimensions {
+                    height: 100%;
+                }
+
+            `]
 })
 export class InputMessageComponent {
     @Output() enterMessage = new EventEmitter<string>();
