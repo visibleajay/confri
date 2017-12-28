35d3d803c1dbf6448e4a56909f1f8a2bdacbbe15
Ajay Kumar <visibleajay@gmail.com>
2017-12-28 18:25:41 +0530

    add reducer.
    add reducer for chat application.

--- confri-angular/src/app/core/confri.state.ts
+++ confri-angular/src/app/core/confri.state.ts
@@ -4,10 +4,11 @@ import { Action } from 'redux';
 export type MESSAGE_STATE_TYPES = 'success' | 'pending' | 'error';
 
 export interface IfcMessage {
+	id: number;
 	text: String;
 	incoming: Boolean;
 	sender: String;
-	sentAt: Date;
+	time: Date;
 	state: MESSAGE_STATE_TYPES;
 }
 
@@ -21,11 +22,29 @@ export const INITIAL_CONFRI_STATE: IfcConfriState = {
 	messageList: []
 }
 
-export function rootReducer(lastState: IfcConfriState, action: Action): IfcConfriState {
+export function rootReducer(lastState: IfcConfriState, action): IfcConfriState {
 	const newState: IfcConfriState = INITIAL_CONFRI_STATE;
 	switch(action.type) {
 		case ConfriActions.USERNAME:
-			return newState;
+			return {
+				...lastState,
+				username: action.username
+			};
+		case ConfriActions.ADD_MESSAGE:
+			return {
+				username: lastState.username,
+				messageList: [...lastState.messageList, action.message]	
+			};
+		case ConfriActions.UPDATE_MESSAGE_STATE:
+			return {
+				...lastState,
+				messageList: lastState.messageList.map( (message) =>{
+					if ( message.id === action.payload.id ) {
+						message['state'] = action.payload.state;
+					}
+					return message;
+				})
+			};
 		default:
 			return newState;
 	}
