cc0ab909202e5e4bbe7f08ae570bb3456ec7161d
Ajay Kumar <visibleajay@gmail.com>
2017-12-30 13:42:34 +0530

    Use immutable.js for state storage.
    Using Immutable library for storing and updating state inside application.

--- confri-angular/src/app/app.component.ts
+++ confri-angular/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material';
-import {Observable} from 'rxjs';
+import { Observable } from 'rxjs';
 import { NgRedux, select } from '@angular-redux/store';
 
 import { UserNameDialogComponent } from './user-name-dialog/user-name-dialog.component';
@@ -16,7 +16,7 @@ import { IfcMessage } from './core/confri.state';
 export class AppComponent implements OnInit{
 
   private username: string;
-  @select() messageList$: Observable<IfcMessage[]>;
+  @select(['messageList'] ) readonly messageList$: Observable<IfcMessage[]>;
 
   constructor(public dialog: MatDialog,
               public confriActions: ConfriActions) {}
--- confri-angular/src/app/core/confri.state.ts
+++ confri-angular/src/app/core/confri.state.ts
@@ -1,5 +1,6 @@
 import { ConfriActions } from './confri.actions';
 import { Action } from 'redux';
+import * as Immutable from 'immutable';
 
 export type MESSAGE_STATE_TYPES = 'success' | 'pending' | 'error';
 
@@ -17,34 +18,42 @@ export interface IfcConfriState {
 	messageList: Array<IfcMessage>;
 }
 
-export const INITIAL_CONFRI_STATE: IfcConfriState = {
+export const INITIAL_CONFRI_STATE: IfcConfriState = Immutable.fromJS({
 	username: '',
 	messageList: []
-}
+} as IfcConfriState)
 
-export function rootReducer(lastState: IfcConfriState, action): IfcConfriState {
+export function rootReducer(lastState, action): IfcConfriState {
 	const newState: IfcConfriState = INITIAL_CONFRI_STATE;
 	switch(action.type) {
 		case ConfriActions.USERNAME:
-			return {
-				...lastState,
-				username: action.username
-			};
+			return lastState.set('username', action.username);
+			// return {
+			// 	...lastState,
+			// 	username: action.username
+			// };
 		case ConfriActions.ADD_MESSAGE:
-			return {
-				username: lastState.username,
-				messageList: [...lastState.messageList, action.message]	
-			};
+			return lastState.updateIn(['messageList'], messageList => messageList.push(action.message));
+			// return {
+			// 	username: lastState.username,
+			// 	messageList: [...lastState.messageList, action.message]	
+			// };
 		case ConfriActions.UPDATE_MESSAGE_STATE:
-			return {
-				...lastState,
-				messageList: lastState.messageList.map( (message) =>{
-					if ( message.id === action.payload.id ) {
-						message['state'] = action.payload.state;
-					}
-					return message;
-				})
-			};
+			return lastState.updateIn(['messageList'], list =>  list.map( (message) => {
+						if ( message.id === action.payload.id){
+							message['state'] = action.payload.id
+						}
+						return message;
+					}));
+			// return {
+			// 	...lastState,
+			// 	messageList: lastState.messageList.map( (message) =>{
+			// 		if ( message.id === action.payload.id ) {
+			// 			message['state'] = action.payload.state;
+			// 		}
+			// 		return message;
+			// 	})
+			// };
 		default:
 			return newState;
 	}
